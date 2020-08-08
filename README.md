9210b9b4d36d6509a4587bd69b5d36e244f8fcbc
rifkiystark <ananda.rifkiy32@gmail.com>
2020-08-08 19:37:51 +0700

    fix links download

--- README.md
+++ README.md
@@ -1,19 +1,22 @@
 ## OtakuDesu API
-__BASE URL__ : https://otakudesu.org/
+
+**BASE URL** : https://otakudesu.org/
+
 ### TODO
+
 - [x] Home
-    - [x] Ongoing
-    - [x] complete
+  - [x] Ongoing
+  - [x] complete
 - [x] Ongoing Pagination
 - [x] Complete Pagination
 - [x] Schedule
 - [x] Genre List
-    - [x] Anime By Genre
+  - [x] Anime By Genre
 - [x] Detail Anime
-    - [x] Detail
-    - [ ] Batch (Can't get link)
-    - [ ] Episode List (Can't get link)
+  - [x] Detail
+  - [ ] Batch (Can't get link)
+  - [x] Episode List (Can't get link)
 - [ ] Detail Batch
-    - [ ] Download Link
+  - [ ] Download Link
 - [ ] Detail Episode
-    - [ ] Download Link
+  - [ ] Download Link
--- controllers/anime.controller.js
+++ controllers/anime.controller.js
@@ -1,50 +1,129 @@
-const url = require('../helpers/base-url')
-const { default: Axios } = require('axios')
-const cheerio = require('cheerio')
-const errors = require('../helpers/errors')
+const url = require("../helpers/base-url");
+const { default: Axios } = require("axios");
+const cheerio = require("cheerio");
+const errors = require("../helpers/errors");
 
-class AnimeController{
-    detailAnime = (req,res)=>{
-        const id = req.params.id
-        const fullUrl = url.baseUrl+`anime/${id}`
-        // console.log(fullUrl);
-        Axios.get(fullUrl).then(response=>{
-            const $ = cheerio.load(response.data)
-            const detailElement = $('.venser').find('.fotoanime')
-            const epsElement = $('.episodelist')
-            let object = {}
-            let episode_list = []
-            object.thumb = detailElement.find('img').attr('src')
-            let genre_name,genre_id,genre_link
-            let genreList = []
-            detailElement.find('.infozin').filter(function(){
-                object.title = $(this).find('p').children().eq(0).text().replace('Judul: ','')
-                object.japanase = $(this).find('p').children().eq(1).text().replace('Japanese: ','')
-                object.score = $(this).find('p').children().eq(2).text().replace('Skor: ','')
-                object.producer = $(this).find('p').children().eq(3).text().replace('Produser:  ','')
-                object.type = $(this).find('p').children().eq(4).text().replace('Tipe: ','')
-                object.status = $(this).find('p').children().eq(5).text().replace('Status: ','')
-                object.total_episode = $(this).find('p').children().eq(6).text().replace('Total Episode: ','')
-                object.duration = $(this).find('p').children().eq(7).text().replace('Durasi: ','')
-                object.release_date = $(this).find('p').children().eq(8).text().replace('Tanggal Rilis: ','')
-                object.studio = $(this).find('p').children().eq(9).text().replace('Studio: ','')
-                $(this).find('p').children().eq(10).find('span > a').each(function() {
-                    genre_name = $(this).text()
-                    genre_id = $(this).attr('href').replace('https://otakudesu.org/genres/','')
-                    genre_link = $(this).attr('href')
-                    genreList.push({genre_name,genre_id,genre_link})
-                    object.genre_list = genreList
-                })
-            })
-            epsElement.eq(0).find('ul').filter(function (){
-                console.log($(this).find('span:nth-child(1) > a').text());
-            })
-            // console.log(epsElement.eq(0).children().last().text());
-            res.json(object)
-        }).catch(err=>{
-            errors.requestFailed(req,res,err)
-        })
+class AnimeController {
+  detailAnime = async (req, res) => {
+    const id = req.params.id;
+    const fullUrl = url.baseUrl + `anime/${id}`;
+    // console.log(fullUrl);
+    try {
+      const response = await Axios.get(fullUrl);
+
+      const $ = cheerio.load(response.data);
+      const detailElement = $(".venser").find(".fotoanime");
+      const epsElement = $("#_epslist").html();
+      let object = {};
+      let episode_list = [];
+      object.thumb = detailElement.find("img").attr("src");
+      let genre_name, genre_id, genre_link;
+      let genreList = [];
+      detailElement.find(".infozin").filter(function () {
+        object.title = $(this)
+          .find("p")
+          .children()
+          .eq(0)
+          .text()
+          .replace("Judul: ", "");
+        object.japanase = $(this)
+          .find("p")
+          .children()
+          .eq(1)
+          .text()
+          .replace("Japanese: ", "");
+        object.score = $(this)
+          .find("p")
+          .children()
+          .eq(2)
+          .text()
+          .replace("Skor: ", "");
+        object.producer = $(this)
+          .find("p")
+          .children()
+          .eq(3)
+          .text()
+          .replace("Produser:  ", "");
+        object.type = $(this)
+          .find("p")
+          .children()
+          .eq(4)
+          .text()
+          .replace("Tipe: ", "");
+        object.status = $(this)
+          .find("p")
+          .children()
+          .eq(5)
+          .text()
+          .replace("Status: ", "");
+        object.total_episode = $(this)
+          .find("p")
+          .children()
+          .eq(6)
+          .text()
+          .replace("Total Episode: ", "");
+        object.duration = $(this)
+          .find("p")
+          .children()
+          .eq(7)
+          .text()
+          .replace("Durasi: ", "");
+        object.release_date = $(this)
+          .find("p")
+          .children()
+          .eq(8)
+          .text()
+          .replace("Tanggal Rilis: ", "");
+        object.studio = $(this)
+          .find("p")
+          .children()
+          .eq(9)
+          .text()
+          .replace("Studio: ", "");
+        $(this)
+          .find("p")
+          .children()
+          .eq(10)
+          .find("span > a")
+          .each(function () {
+            genre_name = $(this).text();
+            genre_id = $(this)
+              .attr("href")
+              .replace("https://otakudesu.org/genres/", "");
+            genre_link = $(this).attr("href");
+            genreList.push({ genre_name, genre_id, genre_link });
+            object.genre_list = genreList;
+          });
+      });
+
+      const plainResponse = $(".venser").html();
+      const startIndex = plainResponse.search(",id:");
+      const id = plainResponse
+        .substr(startIndex, 20)
+        .split(" ")[1]
+        .split("}")[0];
+      const episodesResponse = await Axios.get(
+        `https://otakudesu.org/wp-admin/admin-ajax.php?action=epslist&id=${id}`
+      );
+      const episodes$ = cheerio.load(episodesResponse.data);
+      const links = [];
+      episodes$("body")
+        .find("li")
+        .each((i, elem) => {
+          const $ = cheerio.load(elem);
+          const episode = {
+            name: $("li").find("a").attr("href"),
+            link: $("li").find("a").text().replace("\n", ""),
+          };
+          links.push(episode);
+        });
+      object.links = links;
+      //console.log(epsElement);
+      res.json(object);
+    } catch (err) {
+      errors.requestFailed(req, res, err);
     }
+  };
 }
 
-module.exports = new AnimeController()
+module.exports = new AnimeController();
