9bbd4e7f2dcf9adb9bcefcdb4406cc657ad29312
febryardiansyah <febrymuhammad80@gmail.com>
2020-10-11 10:55:03 +0700

    fix source2

--- helpers/episodeHelper.js
+++ helpers/episodeHelper.js
@@ -6,12 +6,14 @@ const get = async (url) => {
     const $ = cheerio.load(response.data);
     let source1 = $.html().search('"file":');
     let source2 = $.html().search("'file':");
+    console.log(source1);
+    console.log(source2);
     if (source1 !== -1) {
       const end = $.html().indexOf('","');
       return $.html().substring(source1 + 8, end);
-    } else if (source2 === -1) {
+    } else if (source2 !== -1) {
       const end = $.html().indexOf("','");
-      return $.html().substring(source1 + 8, end);
+      return $.html().substring(source2 + 8, end);
     }
     return "-";
   } catch (error) {
