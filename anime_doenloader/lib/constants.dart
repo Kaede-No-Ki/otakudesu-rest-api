ad6eed3020f7559326e9e7470c1608ebb1f25e20
Ferdian <ferdisatria15@gmail.com>
2021-08-08 20:21:22 +0700

    first_commit

---
+++ anime_doenloader/lib/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const primaryColor = Color(0xFF0C9869);
+const textColor = Color(0xFF3C4046);
+const backgroundColor = Color(0xFFF9F8FD);
+
+const double defaultPadding = 20.0 ;
